b67bc81a9d3510e98c643b9142f2d52131712349
Reon Nakamura <82386935+NakamuraReon@users.noreply.github.com>
2025-06-15 07:05:12 +0900

    Update search_test.py

--- src/search_test.py
+++ src/search_test.py
@@ -1,6 +1,6 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://my_neo4j:7687", auth=("neo4j", "password"))
+driver = GraphDatabase.driver("bolt://neo4j:7687", auth=("neo4j", "password"))
 session = driver.session()
 for i in session.run("MATCH (n) RETURN n LIMIT 25"):
     print(i["n"])
